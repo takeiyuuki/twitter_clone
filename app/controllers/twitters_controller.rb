0574631a5cd3d5179f4743b433c791c77d22a6df
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-21 18:58:10 +0900

    blog_paramsメソッドを作成 privateメソッド配下にすることでここのクラスでしかblog_paramsメソッドは使用できないようにした。

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -7,7 +7,13 @@ class TwittersController < ApplicationController
   end
 
   def create
-    Twitter.create(params.require(:blog).permit(:title, :content))
+    Twitter.create(blog_params)
     redirect_to new_twitter_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
