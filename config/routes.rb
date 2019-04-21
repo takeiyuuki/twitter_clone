1a2ad64f2ea50c8e1632bad513986086495d987a
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-21 16:14:58 +0900

    getメソッドで/twittersリクエストを受けた時にtwittersコントローラのindexアクション起動させるコード追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'twitters/index'
+  get '/twitters', to: 'twitters#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
