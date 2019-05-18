0ea9531c4acdccb8aebedc6baff35df9fd9bbc33
yuukitakei <consultation.pharmacist3@gmail.com>
2019-05-18 19:53:33 +0900

    （仮）ログイン機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # ログイン成功した場合
+    else
+      # ログイン失敗した場合
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
   end
 end
