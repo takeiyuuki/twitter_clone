eb8856d60fdcdd38deae22ff0fa1874684462901
yuukitakei <consultation.pharmacist3@gmail.com>
2019-05-18 15:18:54 +0900

    usersテーブルのemailカラムにindexを追加。 emailにユニーク制約を設定。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_071957) do
+ActiveRecord::Schema.define(version: 2019_05_17_003431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "twitters", force: :cascade do |t|
     t.text "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
