844a7d8daba7ffece4a85ecf8f20b1b652601a06
yuukitakei <consultation.pharmacist3@gmail.com>
2019-05-18 20:32:45 +0900

    sessionメソッドを利用してログインを行い、ユーザ詳細ページにリダイレクトさせている（？）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       # ログイン失敗した場合
       flash.now[:danger] = 'ログインに失敗しました'
